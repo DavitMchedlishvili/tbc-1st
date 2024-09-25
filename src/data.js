b3fba3e61454c154bf101be243f46d3615f9fed8
Daviti <davit.mchedlishvili.2396@gmail.com>
2024-09-26 00:32:50 +0400

    build data for cards

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import Word from "./assets/Word.png"
+import Excel from "./assets/Excel.png"
+import PowerPoint from "./assets/PowerPoint.png"
+
+const data = [
+    {
+        title: "Word",
+        description:"    Microsoft Word is a word processing software developed by Microsoft. It allows users to create, edit, and format documents, including text, images, tables, and other elements. Word is widely used for writing letters, reports, essays, and other professional documents. It offers features like spell check, grammar suggestions, templates, and collaborative tools, making it versatile for both personal and business use.",
+        photo: Word,
+        btnColor: "#00A4EF",
+    },
+    {
+        title: "Excel",
+        description:"    Microsoft Word is a word processing software developed by Microsoft. It allows users to create, edit, and format documents, including text, images, tables, and other elements. Word is widely used for writing letters, reports, essays, and other professional documents. It offers features like spell check, grammar suggestions, templates, and collaborative tools, making it versatile for both personal and business use.",
+        photo: Excel,
+        btnColor: "#1D6F42"
+    },
+    {
+        title: "PowerPoint",
+        description:"    Microsoft Word is a word processing software developed by Microsoft. It allows users to create, edit, and format documents, including text, images, tables, and other elements. Word is widely used for writing letters, reports, essays, and other professional documents. It offers features like spell check, grammar suggestions, templates, and collaborative tools, making it versatile for both personal and business use.",
+        photo: PowerPoint,
+        btnColor: "#d04423",
+    },
+];
+
+
+export default data;
