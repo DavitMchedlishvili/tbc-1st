65af7a15eaf54c188b7a4eaa28c92749b6e6d11a
Iamyouandwhatiseeisme <saabashidze@gmail.com>
2024-10-06 02:05:42 +0400

    reft(delete): delete react files from old directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./app/home/page";
+import About from "./app/about/page"
 function App() {
  
   return (
     <>
-    <Home />
+    {/* <Home /> */}
+    <About />
     </>
   )
 }
