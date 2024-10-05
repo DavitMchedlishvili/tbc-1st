a0f9e3151139edce6261d6e153c2bb272beefe6f
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-06 01:19:49 +0400

    add props validation in layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,29 @@
+import PropTypes from 'prop-types';
+import Head from 'next/head';
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 
-
-
 export const metadata = {
-    title: 'Group 5',
-    description: 'Our app is homework just for academy',
-  }
+  title: 'Group 5',
+  description: 'Our app is homework just for academy',
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <link rel="icon" href="/vite.svg" type="image/svg+xml" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
+        <Header />
+        <div id="root">{children}</div>
+        <Footer />
+      </body>
+    </html>
+  );
+}
 
-// eslint-disable-next-line react/prop-types
-export default function RootLayout({children}) {
-    return (<html lang="en">
-        <body>
-          <Header/>
-          <div id="root">{children}</div>
-          <Footer/>
-        </body>
-      </html>);
-  }
+RootLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
