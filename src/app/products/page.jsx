a34eac962f34e9347616f8bce88e37d9ab148d21
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-06 23:08:55 +0400

    add loading spinner animation

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -4,14 +4,18 @@ import { useEffect, useState } from "react";
 import "./index.css";
 
 const ProductFetch = () => {
-  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const res = await fetch("https://dummyjson.com/products");
       const data = await res.json();
 
-      setProduct(data.products);
+      setTimeout(() => {
+        setProducts(data.products);
+        setLoading(false);
+      }, 2000);
     }
 
     fetchData();
@@ -20,18 +24,35 @@ const ProductFetch = () => {
   return (
     <div className="product-page container">
       <h1>Our Products</h1>
-      <div className="product-grid">
-        {product.map((item) => (
-          <div key={item.id} className="product-card">
-            <img src={item.thumbnail} alt={item.title} className="product-image"/>
-            <div className="product-info">
-              <h2>{item.title}</h2>
-              <p className="product-description">{item.description}</p>
-              <p className="product-price">${item.price}</p>
-            </div>
+
+      {loading ? (
+        <div className="loading-placeholder">
+          <div className="spinner"></div>
+
+          <div className="skeleton-grid">
+            {[...Array(6)].map((_, index) => (
+              <div key={index} className="skeleton-card"></div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ) : (
+        <div className="product-grid">
+          {products.map((item) => (
+            <div key={item.id} className="product-card">
+              <img
+                src={item.thumbnail}
+                alt={item.title}
+                className="product-image"
+              />
+              <div className="product-info">
+                <h2>{item.title}</h2>
+                <p className="product-description">{item.description}</p>
+                <p className="product-price">${item.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
