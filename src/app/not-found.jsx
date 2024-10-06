511226aa6268d1f4207010dd84f36dfa629921e0
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-07 02:32:21 +0400

    add styles for not-found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,10 +3,7 @@ import "./styles/global.css";
 
 export default function NotFoundPage() {
   return (
-    <div
-      className="not-found-section"
-      style={{ textAlign: "center", padding: "2rem" }}
-    >
+    <div className="not-found-section">
       <h1>Oops! Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
       <Link href="/" style={{ color: "blue", textDecoration: "underline" }}>
