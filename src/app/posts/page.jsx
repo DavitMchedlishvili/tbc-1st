f70ac8c994aa129e66b291e02cd7d9c498b607f7
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-07 00:40:11 +0400

    add skeleton placeholder

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,39 +1,40 @@
-'use client'
+'use client';
 
-import React, { useEffect, useState } from 'react'
-import "./index.css";
-import Like from "../../../public/assets/like.png";
-import Dislike from "../../../public/assets/dislike.png"
-
+import { useEffect, useState } from 'react';
+import './index.css';
+import Like from '../../../public/assets/like.png';
+import Dislike from '../../../public/assets/dislike.png';
 
 const PostsFetch = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true); 
 
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch('https://dummyjson.com/posts');
+      const data = await res.json();
 
-    
+      setPosts(data.posts);
+      setLoading(false); 
+    }
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-      async function fetchData() {
-        const res = await fetch('https://dummyjson.com/posts');
-        const data = await res.json();
-        
-        setPosts(data.posts);
-      }
-      
-      fetchData();
-    }, []);
-
-    console.log(posts);
-    
   return (
     <div className="postContainer">
       <h1 className="postTitle">Posts</h1>
-      <div className="posts">
-        {posts.map((post) => (
-          <div key={post.id} className="post">
-            <h2 className="postTitle">{post.title}</h2>
-            <p className="postContent">{post.body}</p>
-            <div className="reactions">
+      
+      {loading ? (
+        <div className="spinner-container">
+          <div className="spinner"></div> 
+        </div>
+      ) : (
+        <div className="posts">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <h2 className="postTitle">{post.title}</h2>
+              <p className="postContent">{post.body}</p>
+              <div className="reactions">
                 <div className="like">
                   <img src={Like.src} alt="like" />
                   <p>{post.reactions.likes}</p>
@@ -42,19 +43,19 @@ const PostsFetch = () => {
                   <img src={Dislike.src} alt="dislike" />
                   <p>{post.reactions.dislikes}</p>
                 </div>
+              </div>
+              <p className="postTags">
+                Tags: {post.tags.map((tag, index) => (
+                  <span key={index}>#{tag}</span>
+                ))}{' '}
+              </p>
+              <p className="postViwes">views: {post.views}</p>
             </div>
-            <p className="postTags">Tags: {post.tags.map((tag, index) => (
-              <span key={index}>
-                #{tag}
-              </span>
-              ))} </p>  
-            <p className="postViwes">views: {post.views} </p>
-  
-          </div>
-        ))}
+          ))}
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PostsFetch
+export default PostsFetch;
