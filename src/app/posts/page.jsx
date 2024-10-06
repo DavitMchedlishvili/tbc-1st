77bf17f762e641ffff53fed0b15d94d26a3b976f
Ani Ananiashvili <ananiashvili10@gmail.com>
2024-10-07 00:54:29 +0400

    delete skeleton placeholders

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import './index.css';
-import Like from '../../../public/assets/like.png';
-import Dislike from '../../../public/assets/dislike.png';
+import { useEffect, useState } from "react";
+import "./index.css";
+import Like from "../../../public/assets/like.png";
+import Dislike from "../../../public/assets/dislike.png";
 
 const PostsFetch = () => {
   const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch('https://dummyjson.com/posts');
+      const res = await fetch("https://dummyjson.com/posts");
       const data = await res.json();
 
       setPosts(data.posts);
-      setLoading(false); 
+      setLoading(false);
     }
     fetchData();
   }, []);
@@ -23,10 +23,18 @@ const PostsFetch = () => {
   return (
     <div className="postContainer">
       <h1 className="postTitle">Posts</h1>
-      
+
       {loading ? (
-        <div className="spinner-container">
-          <div className="spinner"></div> 
+        <div className="skeleton-posts">
+          {[...Array(5)].map((_, index) => (
+            <div key={index} className="skeleton-post">
+              <div className="skeleton-title"></div>
+              <div className="skeleton-content"></div>
+              <div className="skeleton-reactions"></div>
+              <div className="skeleton-tags"></div>
+              <div className="skeleton-views"></div>
+            </div>
+          ))}
         </div>
       ) : (
         <div className="posts">
@@ -45,9 +53,10 @@ const PostsFetch = () => {
                 </div>
               </div>
               <p className="postTags">
-                Tags: {post.tags.map((tag, index) => (
+                Tags:{" "}
+                {post.tags.map((tag, index) => (
                   <span key={index}>#{tag}</span>
-                ))}{' '}
+                ))}{" "}
               </p>
               <p className="postViwes">views: {post.views}</p>
             </div>
