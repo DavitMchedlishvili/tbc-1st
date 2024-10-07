bc0d4aa46ceec2694c8e9f44ecbd0f5c3395a89b
Daviti <davit.mchedlishvili.2396@gmail.com>
2024-10-07 22:53:37 +0400

    final corrections: styles

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <header>
       <div className="header-container container">
-        <Link className='logo' href="/">Home</Link>
+        <Link className='nav-link' href="/">Home</Link>
         <nav >
           <div className='navigation'>
-            <Link href="/products">Products</Link>
-            <Link href="/posts">Posts</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact Us</Link>
+            <Link className='nav-link' href="/products">Products</Link>
+            <Link className='nav-link' href="/blog">Blog</Link>
+            <Link className='nav-link' href="/posts">Posts</Link>
+            <Link className='nav-link' href="/about">About</Link>
+            <Link className='nav-link' href="/contact">Contact Us</Link>
           </div>
         </nav>
         <Link className='profile-btn' href="/profile">Profile</Link>
